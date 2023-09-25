18d94eaaa7291acd22fa4e5ff10f5e6d27f409f5
Alexis G <alexis.goupillot.dev@gmail.com>
2023-09-25 10:06:41 +0200

    Update baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://movies-test.abes.fr',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/doc/',
 
   onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
